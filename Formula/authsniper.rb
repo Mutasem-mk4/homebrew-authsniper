ad0679afb7083b982ac656cdff53ca37ee94b1b6
Mutasem-mk4 <140179052+Mutasem-mk4@users.noreply.github.com>
2026-04-08 02:30:00 +0300

    feat: Initial Homebrew Formula for AuthSniper v1.0.0

---
+++ Formula/authsniper.rb
@@ -0,0 +1,27 @@
+class Authsniper < Formula
+  desc "The Ultimate AST-Driven API BOLA/IDOR Hunter"
+  homepage "https://github.com/Mutasem-mk4/AuthSniper"
+  version "1.0.0"
+
+  if OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Darwin_x86_64.tar.gz"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Darwin_arm64.tar.gz"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Linux_x86_64.tar.gz"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Linux_arm64.tar.gz"
+    end
+  end
+
+  def install
+    bin.install "authsniper"
+  end
+
+  test do
+    system "#{bin}/authsniper", "-h"
+  end
+end
