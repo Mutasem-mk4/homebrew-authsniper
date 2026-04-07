71f78eb80aecff44228389b98bc3b8cc75afc97d
Mutasem-mk4 <140179052+Mutasem-mk4@users.noreply.github.com>
2026-04-08 02:31:36 +0300

    fix: Update formula for v1.0.1

--- Formula/authsniper.rb
+++ Formula/authsniper.rb
@@ -1,19 +1,19 @@
 class Authsniper < Formula
   desc "The Ultimate AST-Driven API BOLA/IDOR Hunter"
   homepage "https://github.com/Mutasem-mk4/AuthSniper"
-  version "1.0.0"
+  version "1.0.1"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Darwin_x86_64.tar.gz"
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.1/authsniper_Darwin_x86_64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Darwin_arm64.tar.gz"
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.1/authsniper_Darwin_arm64.tar.gz"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Linux_x86_64.tar.gz"
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.1/authsniper_Linux_x86_64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.0/authsniper_Linux_arm64.tar.gz"
+      url "https://github.com/Mutasem-mk4/AuthSniper/releases/download/v1.0.1/authsniper_Linux_arm64.tar.gz"
     end
   end
 
